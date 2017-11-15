478e4097bc97d46c8321d623cd17b585aff8dde2
Damir Isanbirdin <damir.isanbirdin@gmail.com>
2017-11-15 21:31:41 +0100

    use double instead Decimal

--- ta4j-core/src/main/java/org/ta4j/core/MathUtils.java
+++ ta4j-core/src/main/java/org/ta4j/core/MathUtils.java
@@ -9,14 +9,19 @@ public class MathUtils {
     public final static double DELTA = 0.00001;
 
     public static boolean isEqual(double d1, double d2) {
-        long a1 = (long) d1 * PRECISION;
-        long a2 = (long) d2 * PRECISION;
+        long a1 = (long) (double) (d1 * PRECISION);
+        long a2 = (long) (double) (d2 * PRECISION);
         return Long.compare(a1, a2) == 0;
     }
 
     public static boolean isZero(double d1) {
-        long a1 = (long) d1 * PRECISION;
+        long a1 = (long) (double) (d1 * PRECISION);
         return a1 == 0;
     }
 
+
+    public static void main(String[] args) {
+        boolean q = isEqual(0.004d, 0.0004d);
+        System.out.println("q = " + q);
+    }
 }
